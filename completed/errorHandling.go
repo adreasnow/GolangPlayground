0fd43884a9334fd7e9ea7088c5c549a1e2ff1d21
Adrea Snow <adrea.snow@gmail.com>
2023-09-30 13:25:49 +1000

    error handling example

---
+++ completed/errorHandling.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Throws an error if you input the value 3
+func funcName(n int) (int, error) {
+	if n == 3 {
+		return 0, fmt.Errorf("how DARE you use the number 3")
+	}
+	return n, nil
+}
+
+// Catches the error from funcName()
+func testFunc(n int) {
+	num1, err := funcName(n)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	fmt.Printf("num1=%#v", num1)
+}
+
+func main() {
+	testFunc(3)
+	testFunc(5)
+}
